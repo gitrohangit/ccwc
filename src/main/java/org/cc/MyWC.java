9b8287b08afb2cc4ac2c00ae06301090463b291a
Rohan Chopra <rohanchopra@atom-20.mynetworksettings.com>
2024-03-13 03:17:00 -0400

    Implemented -l option

--- src/main/java/org/cc/MyWC.java
+++ src/main/java/org/cc/MyWC.java
@@ -1,17 +1,17 @@
 package org.cc;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class MyWC {
     public static void main(String[] args) {
-        if (args.length < 2 || !args[0].equals("-c")) {
-            System.out.println("Usage: ccwc -c <filename>");
+        if (args.length < 3 || !args[0].startsWith("ccwc") || !args[1].startsWith("-")) {
+            System.out.println("Usage: ccwc [-c | -l] <filename>");
             return;
         }
 
-        String filename = args[1];
+        String option = args[1];
+        String filename = args[2];
+
         File file = new File(filename);
 
         if (!file.exists()) {
@@ -19,9 +19,19 @@ public class MyWC {
             return;
         }
 
-        long byteCount = countBytes(file);
-
-        System.out.println(byteCount + " " + filename);
+        long count;
+        switch (option) {
+            case "-c":
+                count = countBytes(file);
+                System.out.println(count + " " + filename);
+                break;
+            case "-l":
+                count = countLines(file);
+                System.out.println(count + " " + filename);
+                break;
+            default:
+                System.out.println("Invalid option: " + option);
+        }
     }
 
     private static long countBytes(File file) {
@@ -46,5 +56,17 @@ public class MyWC {
             }
         }
         return byteCount;
+    }
+
+    private static long countLines(File file) {
+        long lineCount = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while (reader.readLine() != null) {
+                lineCount++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lineCount;
     }
 }
